76d804853176c86643b2eaff319e5a7289f7c4e4
Ivan Ivković <ivkovic.ivan@outlook.com>
2021-09-06 22:20:59 +0200

    First homework.

---
+++ dz01-rjesenja.sh
@@ -0,0 +1,13 @@
+pwd
+cat ~/.bash_history
+ls --sort=size --reverse -l -h ~
+mkdir '/tmp/OKOSL tjedan' '/tmp/OKOSL tjedan/ponedjeljak' '/tmp/OKOSL tjedan/ponedjeljak' '/tmp/OKOSL tjedan/utorak' '/tmp/OKOSL tjedan/srijeda' '/tmp/OKOSL tjedan/cetvrtak' '/tmp/OKOSL tjedan/petak' '/tmp/OKOSL tjedan/.subota'
+touch '/tmp/OKOSL tjedan/predavanja' '/tmp/OKOSL tjedan/labosi' '/tmp/OKOSL tjedan/zadaca1' '/tmp/OKOSL tjedan/zadaca2' '/tmp/OKOSL tjedan/zadaca3' '/tmp/OKOSL tjedan/zadaca4' '/tmp/OKOSL tjedan/zadaca5' '/tmp/OKOSL tjedan/zadaca6' '/tmp/OKOSL tjedan/zadaca7' '/tmp/OKOSL tjedan/zadaca8'
+cp -a '/tmp/OKOSL tjedan/.subota/' '/tmp/OKOSL tjedan/ponedjeljak'
+ls -l --recursive -a /tmp/OKOSL\ tjedan/
+ln -s /var/ ~/Varionica
+df -h /
+file /bin/
+file /etc/passwd
+file /boot/
+
