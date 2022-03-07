56201775b625712bf25e10b99b47a2a042704717
Ivan Ivković <ivkovic.ivan@outlook.com>
2022-03-07 20:45:22 +0100

    Homework 3 finished.

--- dz03-rjesenja.sh
+++ dz03-rjesenja.sh
@@ -11,3 +11,5 @@ cp /usr/share/dict/words .
 sed -r "s/(^.*)'s$/\1s/" ./words > ./words-2
 sed -r "s/^(.*)word(.*)$/word\1\2/" ./words-2 > ./words-3
 sed -r "s/^([A-Z])(.*)([A-Za-z])$/\L&\1\2\U&\3/" ./words > ./words-4
+sed -e 's/\(.*\)/\L\1/' ./Top10
+sed -e 's/\([aeiou]\)/\U\1/g' ./Top10
